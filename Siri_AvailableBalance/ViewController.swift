a4b79d04f6173ab38bbf873a989284cf313cdda0
Robert D'Almeida <robert.dalmeida@ing.com>
2018-11-19 15:54:23 +0100

    Initial Commit

---
+++ Siri_AvailableBalance/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Siri_AvailableBalance
+//
+//  Created by Robert D Almeida on 19/11/2018.
+//  Copyright © 2018 Robert D Almeida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
